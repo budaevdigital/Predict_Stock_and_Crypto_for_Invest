a0a54e02f7fcce47caa411040d1d4f8e4bdc34e0
budaevdigital <budaev.digital@gmail.com>
2022-06-14 01:51:48 +0500

    добавил логирование переменных окружения

--- predict_stock_crypto/config/settings.py
+++ predict_stock_crypto/config/settings.py
@@ -6,8 +6,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Загрузим переменные окружения
-load_dotenv()
+
 
 # Выбираем абсолютный путь - берём за основу этот файл и поднимаемся на две директории выше
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,6 +18,13 @@ CONFIG_DIR = os.path.join(BASE_DIR, 'config')
 LOGGING_DIR = os.path.join(CONFIG_DIR, 'logging')
 LOGGING = os.path.join(LOGGING_DIR, 'logfile.log')
 
+# Здесь задана глобальная конфигурация для всех логгеров 
+logging.basicConfig(
+        handlers=[RotatingFileHandler(LOGGING, maxBytes=100000, backupCount=5)],
+        level=logging.ERROR,
+        format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+        datefmt='%Y-%m-%d | T%H:%M:%S')
+
 # Количество избранных валют у каждого пользователя
 WATCHLIST_POSITION = 3
 
@@ -27,15 +33,17 @@ COUNTRY = 'United States'
 # специальное отображение графиков для pyplot fivethirtyeight
 PYPLOT_SET_GRAPH = 'fivethirtyeight'
 
+
+# Загрузим переменные окружения
+load_dotenv()
 # При работе с переменными окружения, будем использовать getenv
 # В отличии от environ, getenv не вызывает исключения,
 # а возвращает None
-TOKEN = os.getenv('TOKEN')
-CHAT_ID = int(os.getenv('CHATID'))
-
-# Здесь задана глобальная конфигурация для всех логгеров 
-logging.basicConfig(
-        handlers=[RotatingFileHandler(LOGGING, maxBytes=100000, backupCount=5)],
-        level=logging.ERROR,
-        format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
-        datefmt='%Y-%m-%d | T%H:%M:%S')
+try:
+    TOKEN = os.getenv('TOKEN')
+except Exception as error:
+    logging.critical(f'({error}) - отсутствует переменная окружения "TOKEN"') 
+try:
+    CHAT_ID = int(os.getenv('CHATID'))
+except Exception as error:
+    logging.critical(f'({error}) - отсутствует переменная окружения "CHAT_ID"') 
